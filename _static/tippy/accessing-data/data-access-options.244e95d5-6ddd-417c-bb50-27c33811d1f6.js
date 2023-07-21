b81c6a31a3fbc3e7ea6abd715148d492ede28d99
wragge <wragge@users.noreply.github.com>
2023-07-21 07:54:03 +0000

    deploy: b4e06dad38319e28e1cd6db572068eedda9966a9

---
+++ _static/tippy/accessing-data/data-access-options.244e95d5-6ddd-417c-bb50-27c33811d1f6.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#downloading-pre-harvested-datasets\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">8.2. </span>Downloading pre-harvested datasets<a class=\"headerlink\" href=\"#downloading-pre-harvested-datasets\" title=\"Permalink to this heading\">#</a></h2>", "a[href=\"#using-the-web-interface\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">8.1. </span>Using the web interface<a class=\"headerlink\" href=\"#using-the-web-interface\" title=\"Permalink to this heading\">#</a></h2>", "a[href=\"#querying-apis\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">8.3. </span>Querying APIs<a class=\"headerlink\" href=\"#querying-apis\" title=\"Permalink to this heading\">#</a></h2>", "a[href=\"#data-access-options\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">8. </span>Data access options<a class=\"headerlink\" href=\"#data-access-options\" title=\"Permalink to this heading\">#</a></h1><p>This section documents methods for obtaining <span class=\"xref std std-term\">machine-readable data</span> from Trove. You can manually download some types of data, such as images, from Trove\u2019s web interface. But the focus here is on reusable and extensible methods that can be used in a variety of workflows, such as the creation of datasets for further analysis.</p><p>There are a number of ways in which you can access data from Trove:</p>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
