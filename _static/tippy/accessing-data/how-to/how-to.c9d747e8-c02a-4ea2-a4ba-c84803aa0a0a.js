9ba8365435e971ee6b59ef8e799a05c891f68e94
wragge <wragge@users.noreply.github.com>
2023-10-27 03:06:04 +0000

    deploy: 5c532fa9a5ae31a2d2ce0de60a884a4183492697

---
+++ _static/tippy/accessing-data/how-to/how-to.c9d747e8-c02a-4ea2-a4ba-c84803aa0a0a.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"use-zotero.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">18.4. </span>HOW TO: Use Zotero to save items<a class=\"headerlink\" href=\"#how-to-use-zotero-to-save-items\" title=\"Permalink to this heading\">#</a></h1><p><mark>==If I rewrite the translator to use the API I can expand this section to include other content types==</mark></p>", "a[href=\"download-higher-resolution-images.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">18.1. </span>HOW TO: Download higher resolution versions of images from the web interface<a class=\"headerlink\" href=\"#how-to-download-higher-resolution-versions-of-images-from-the-web-interface\" title=\"Permalink to this heading\">#</a></h1><p>One confusing, and often frustrating, aspect of <a class=\"reference internal\" href=\"../using-web-interface.html#downloading-images-web-interface\"><span class=\"std std-ref\">image downloads via the Trove web interface</span></a> is their resolution. You can use the Trove image viewer to zoom right in to many photographs and manuscripts, enabling you to pick up fine details. But if you download the same image you could find the resolution is much lower. This means you\u2019re limited in how you can use the downloaded image. The available resolutions vary across categories and formats, and you really don\u2019t know what you\u2019ll get until you download it. Many manuscripts, in particular, seem to have low-resolution downloads, which doesn\u2019t help you much when you\u2019re trying to decipher someone\u2019s handwriting!</p><p>Here\u2019s a couple of ways to get higher resolution versions.</p>", "a[href=\"use-bulk-export.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">18.2. </span>HOW TO: Use the bulk export option to save the results of a search<a class=\"headerlink\" href=\"#how-to-use-the-bulk-export-option-to-save-the-results-of-a-search\" title=\"Permalink to this heading\">#</a></h1><p><mark>==Include more info once official documentation is available==</mark></p><p><mark>==Note that there are some differences in fields from the API==</mark></p>", "a[href=\"harvest-complete-results.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">18.5. </span>HOW TO: Harvest a complete set of search results using the Trove API<a class=\"headerlink\" href=\"#how-to-harvest-a-complete-set-of-search-results-using-the-trove-api\" title=\"Permalink to this heading\">#</a></h1><p>See <a class=\"reference internal\" href=\"../trove-api-intro.html\"><span class=\"doc\">Trove API introduction</span></a> for general information about using the Trove API.</p>", "a[href=\"use-lists.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">18.3. </span>HOW TO: Use Trove lists to create a dataset<a class=\"headerlink\" href=\"#how-to-use-trove-lists-to-create-a-dataset\" title=\"Permalink to this heading\">#</a></h1><p>Another option for creating collections of manually selected newspaper article metadata is Trove\u2019s built-in Lists function.</p><p><mark>==Note about adding mutliple items to Lists. Hacking the url to get more==</mark></p>", "a[href=\"#how-to\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">18. </span>How to<a class=\"headerlink\" href=\"#how-to\" title=\"Permalink to this heading\">#</a></h1>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
---
+++ _static/tippy/other-digitised-resources/how-to/digitised-resources.fd4a33dc-5ca9-41d6-bb09-017a0a3118f2.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"get-collection-items.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">31.1. </span>HOW TO: Get a list of items in a digitised collection<a class=\"headerlink\" href=\"#how-to-get-a-list-of-items-in-a-digitised-collection\" title=\"Permalink to this heading\">#</a></h1><p>The NLA\u2019s digitised resources are often presented as \u2018collections\u2019. A collection could be the volumes in a multi-volume work, the issues of a periodical, a map series, an album of photographs, or a manuscript collection. In the web interface, collections will have a \u2018Browse this collection\u2019 link or button that displays a list of the contents, but getting machine-readable data is not so straightforward. You can use the <code class=\"docutils literal notranslate\"><span class=\"pre\">magazine/title</span></code> API endpoint to get a list of issues from a periodical, but there\u2019s no way to get the contents of other types of collections from the Trove API.</p><p><mark>==Insert screencap==</mark></p>", "a[href=\"download-items-text-images.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">31.2. </span>HOW TO: Automate the download of digitised items as text, images, or PDFs<a class=\"headerlink\" href=\"#how-to-automate-the-download-of-digitised-items-as-text-images-or-pdfs\" title=\"Permalink to this heading\">#</a></h1><p>You can download text, images, and PDFs from individual digitised items <a class=\"reference internal\" href=\"../../accessing-data/using-web-interface.html\"><span class=\"doc std std-doc\">using the Trove web interface</span></a>. But only the text of periodical articles is available for machine access through the Trove API. This makes it difficult to assemble datasets, or build processing pipelines involving digitised resources. This page documents a series of work arounds that enable you to automate the download of digitised items as text, images, or PDFs.</p>", "a[href=\"#digitised-resources\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">31. </span>Digitised resources<a class=\"headerlink\" href=\"#digitised-resources\" title=\"Permalink to this heading\">#</a></h1>", "a[href=\"extract-embedded-metadata.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">31.3. </span>HOW TO: Extract additional metadata from the web page of a digitised work<a class=\"headerlink\" href=\"#how-to-extract-additional-metadata-from-the-web-page-of-a-digitised-work\" title=\"Permalink to this heading\">#</a></h1><p>The viewers you use to examine digitised resources in Trove embed some metadata that isn\u2019t available through the Trove API. This includes a JSON-ified version of the item\u2019s MARC record (presumably copied from the NLA catalogue), as well as structural information used by the viewer itself, such as a list of pages in a digitised book.</p><p>This metadata can be useful in a number of different contexts. For example, you can extract the number of pages in a digitised book, then use this number to <a class=\"reference internal\" href=\"download-items-text-images.html\"><span class=\"doc std std-doc\">automatically download the full text or a PDF</span></a>. The GLAM Workbench includes an example where geospatial coordinates are extracted from the MARC data to add to a <a class=\"reference external\" href=\"https://glam-workbench.net/trove-maps/exploring-digitised-maps/\">harvest of digitised maps</a>.</p>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
