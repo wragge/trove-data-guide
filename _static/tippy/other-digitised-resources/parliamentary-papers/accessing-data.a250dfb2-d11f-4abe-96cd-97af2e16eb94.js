0ae385453a9858b5324b78a72b376a12719c6ff9
wragge <wragge@users.noreply.github.com>
2023-11-20 01:46:41 +0000

    deploy: f03f099e7d1f6a73065a3017081de25c312a9481

---
+++ _static/tippy/other-digitised-resources/parliamentary-papers/accessing-data.a250dfb2-d11f-4abe-96cd-97af2e16eb94.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#accessing-ocrd-text\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Accessing OCRd text<a class=\"headerlink\" href=\"#accessing-ocrd-text\" title=\"Permalink to this heading\">#</a></h2>", "a[href=\"#accessing-images\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Accessing images<a class=\"headerlink\" href=\"#accessing-images\" title=\"Permalink to this heading\">#</a></h2>", "a[href=\"#accessing-metadata\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Accessing metadata<a class=\"headerlink\" href=\"#accessing-metadata\" title=\"Permalink to this heading\">#</a></h2>", "a[href=\"#accessing-data-from-parliamentary-papers\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">3.3. </span>Accessing data from Parliamentary Papers<a class=\"headerlink\" href=\"#accessing-data-from-parliamentary-papers\" title=\"Permalink to this heading\">#</a></h1><h2>What data is available?<a class=\"headerlink\" href=\"#what-data-is-available\" title=\"Permalink to this heading\">#</a></h2><p>The following data is available relating to the digitised Parliamentary Papers in Trove:</p>", "a[href=\"#what-data-is-available\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">What data is available?<a class=\"headerlink\" href=\"#what-data-is-available\" title=\"Permalink to this heading\">#</a></h2><p>The following data is available relating to the digitised Parliamentary Papers in Trove:</p>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
