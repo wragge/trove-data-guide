6e7a1e4ce4f707da27fbb6b4cd6643f3957c1340
wragge <wragge@users.noreply.github.com>
2023-11-20 00:11:04 +0000

    deploy: c184926a836e702ac2a14e4e8df7165d384c99f3

---
+++ _static/tippy/accessing-data/how-to/use-bulk-export.cebcd353-53ca-4261-8a08-9c63fb179c56.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#how-to-use-the-bulk-export-option-to-save-the-results-of-a-search\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">5.2. </span>HOW TO: Use the bulk export option to save the results of a search<a class=\"headerlink\" href=\"#how-to-use-the-bulk-export-option-to-save-the-results-of-a-search\" title=\"Permalink to this heading\">#</a></h1><p><mark>==Include more info once official documentation is available==</mark></p><p><mark>==Note that there are some differences in fields from the API==</mark></p>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
---
+++ _static/tippy/what-is-trove/collections.37e37017-35ef-42a7-b9dd-b966d6707c8d.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#collections-within-collections\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Collections within collections<a class=\"headerlink\" href=\"#collections-within-collections\" title=\"Permalink to this heading\">#</a></h1><h2>isPartOf<a class=\"headerlink\" href=\"#ispartof\" title=\"Permalink to this heading\">#</a></h2>", "a[href=\"#finding-aids\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Finding aids<a class=\"headerlink\" href=\"#finding-aids\" title=\"Permalink to this heading\">#</a></h2><p>Describe the different ways that \u2018collections\u2019 are described within Trove.</p><p>Parent-child relationships in Trove metadata:</p>", "a[href=\"#digitised-collections\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Digitised collections<a class=\"headerlink\" href=\"#digitised-collections\" title=\"Permalink to this heading\">#</a></h2>", "a[href=\"#ispartof\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">isPartOf<a class=\"headerlink\" href=\"#ispartof\" title=\"Permalink to this heading\">#</a></h2>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
