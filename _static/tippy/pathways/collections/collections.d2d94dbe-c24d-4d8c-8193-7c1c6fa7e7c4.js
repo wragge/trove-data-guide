1fe469fb9e613fd0da8c018364ed0de0183169b8
wragge <wragge@users.noreply.github.com>
2024-06-06 08:20:14 +0000

    deploy: 7f35a30056a76414c2509f03b6e7d522497c880e

---
+++ _static/tippy/pathways/collections/collections.d2d94dbe-c24d-4d8c-8193-7c1c6fa7e7c4.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#creating-collections\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">32. </span>Creating collections<a class=\"headerlink\" href=\"#creating-collections\" title=\"Link to this heading\">#</a></h1><p>Tools and methods for assembling, managing, and sharing collections of resources harvested from Trove.</p>", "a[href=\"examples.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">32.1. </span>Tutorials and examples<a class=\"headerlink\" href=\"#tutorials-and-examples\" title=\"Link to this heading\">#</a></h1><h2>Tutorials<a class=\"headerlink\" href=\"#tutorials\" title=\"Link to this heading\">#</a></h2>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
---
+++ _static/tippy/pathways/collections/data-sources.7fd5435e-5db8-47e4-8c46-afcf70ed8d76.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#documentation\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Documentation<a class=\"headerlink\" href=\"#documentation\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#data-sources\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Data sources<a class=\"headerlink\" href=\"#data-sources\" title=\"Link to this heading\">#</a></h1><h2>Documentation<a class=\"headerlink\" href=\"#documentation\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#pre-harvested-data-sets\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Pre-harvested data sets<a class=\"headerlink\" href=\"#pre-harvested-data-sets\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#creating-datasets\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Creating datasets<a class=\"headerlink\" href=\"#creating-datasets\" title=\"Link to this heading\">#</a></h2>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
