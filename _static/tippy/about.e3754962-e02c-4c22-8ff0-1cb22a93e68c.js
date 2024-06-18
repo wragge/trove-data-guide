afad4396d245654db1439d625dde42b9bf957dd5
wragge <wragge@users.noreply.github.com>
2024-06-18 01:27:00 +0000

    deploy: 84a906551a20eb4f95198848c826daa664e805e4

---
+++ _static/tippy/about.e3754962-e02c-4c22-8ff0-1cb22a93e68c.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#development-and-feedback\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Development and feedback<a class=\"headerlink\" href=\"#development-and-feedback\" title=\"Link to this heading\">#</a></h2><p>More content will be added to the Trove Data Guide over coming months. To view work-in-progress see the <a class=\"reference external\" href=\"https://wragge.github.io/trove-data-guide/\">development version</a> and the project\u2019s <a class=\"reference external\" href=\"https://github.com/wragge/trove-data-guide\">GitHub repository</a>.</p><p>Ideas, corrections, and new content are very welcome \u2013 find out more about <a class=\"reference internal\" href=\"contributing.html\"><span class=\"std std-doc\">contributing to the Trove Data Guide</span></a>.</p>", "a[href=\"#glam-workbench\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">GLAM Workbench<a class=\"headerlink\" href=\"#glam-workbench\" title=\"Link to this heading\">#</a></h2><p>The Trove Data Guide is a companion to the Trove sections of the <a class=\"reference external\" href=\"https://glam-workbench.net/\">GLAM Workbench</a>. While the GLAM Workbench provides tools and examples for working with data from Trove, this Guide digs deeper into the complexities to help researchers develop a critical understanding of Trove data \u2013 its limits and its possibilities.</p>", "a[href=\"#about-this-guide\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">About this Guide<a class=\"headerlink\" href=\"#about-this-guide\" title=\"Link to this heading\">#</a></h1><p>The Trove Data Guide is being developed by <a class=\"reference external\" href=\"https://timsherratt.org\">Tim Sherratt</a> as part of the <a class=\"reference external\" href=\"https://ardc.edu.au/project/ardc-community-data-lab/\">ARDC Community Data Lab</a>.</p><p>The published version is available at: <a class=\"reference external\" href=\"https://tdg.glam-workbench.net\">https://tdg.glam-workbench.net</a>.</p>", "a[href=\"contributing.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Contributing to the Trove Data Guide<a class=\"headerlink\" href=\"#contributing-to-the-trove-data-guide\" title=\"Link to this heading\">#</a></h1><p>The Trove Data guide will continue to expand and evolve over time. Your contributions are very welcome!</p>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
---
+++ _static/tippy/born-digital/web-archives/memento.0d1f7167-0e46-45d6-b3a9-56d18ec97f20.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#timegates-timemaps-and-mementos\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Timegates, Timemaps, and Mementos<a class=\"headerlink\" href=\"#timegates-timemaps-and-mementos\" title=\"Link to this heading\">#</a></h1><p>The NLA has frequently said that the Australian Web Archive has no API. This is not strictly true. You can\u2019t retrieve search results from the AWA in a machine-readable form as you can with other Trove categories. But the software used by the AWA supports the <a class=\"reference external\" href=\"https://datatracker.ietf.org/doc/html/rfc7089\">Memento protocol</a> and allows requests for certain types of machine-readable information. It\u2019s just baked in to the system.</p><p>In particular, the AWA supports two Memento-compliant API requests:</p>", "a[href=\"#timemap\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Timemap<a class=\"headerlink\" href=\"#timemap\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#timegates\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Timegates<a class=\"headerlink\" href=\"#timegates\" title=\"Link to this heading\">#</a></h2><p>The <code class=\"docutils literal notranslate\"><span class=\"pre\">Link</span></code> parameter contains the Memento information. You can see that it\u2019s actually providing information on four types of link:</p>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
