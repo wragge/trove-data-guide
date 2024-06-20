7048c4452cf8cfc9353a79692e8c094d5d30c002
wragge <wragge@users.noreply.github.com>
2024-06-20 07:55:10 +0000

    deploy: f6116113ad580ab230ed1d9a7b8f9a3ee3f2e678

---
+++ _static/tippy/pathways/text/examples.c1e71e66-b4f2-47c4-af82-cceee77dbc78.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#tutorials-and-examples\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">27.3. </span>Tutorials and examples<a class=\"headerlink\" href=\"#tutorials-and-examples\" title=\"Link to this heading\">#</a></h1><h2>Tutorials<a class=\"headerlink\" href=\"#tutorials\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#examples-from-the-glam-workbench\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Examples from the GLAM Workbench<a class=\"headerlink\" href=\"#examples-from-the-glam-workbench\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#other-examples\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Other examples<a class=\"headerlink\" href=\"#other-examples\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#tutorials\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Tutorials<a class=\"headerlink\" href=\"#tutorials\" title=\"Link to this heading\">#</a></h2>", "a[href=\"newspapers-keywords.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Analysing keywords in Trove\u2019s digitised newspapers<a class=\"headerlink\" href=\"#analysing-keywords-in-troves-digitised-newspapers\" title=\"Link to this heading\">#</a></h1><p>You want to explore differences in language use across a collection of digitised newspaper articles. The <a class=\"reference external\" href=\"https://www.atap.edu.au/\">Australian Text Analytics Platform</a> provides a <a class=\"reference external\" href=\"https://github.com/Australian-Text-Analytics-Platform/keywords-analysis\">Keywords Analysis tool</a> that helps you examine whether particular words are over or under-represented across collections of text. But how do get data from Trove\u2019s newspapers to the keyword analysis tool?</p>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
---
+++ _static/tippy/understanding-search/notes.6dd3a6a4-17de-4d79-a5f1-b3e8e3400681.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#understanding-search\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Understanding search<a class=\"headerlink\" href=\"#understanding-search\" title=\"Link to this heading\">#</a></h1>", "a[href=\"#searching-for-articles-on-a-specific-page\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Searching for articles on a specific page<a class=\"headerlink\" href=\"#searching-for-articles-on-a-specific-page\" title=\"Link to this heading\">#</a></h1><p>You can use the <code class=\"docutils literal notranslate\"><span class=\"pre\">firstpageseq</span></code> index to search for articles published on a particular page. For example, include <code class=\"docutils literal notranslate\"><span class=\"pre\">firstpageseq:1</span></code> in your query to find articles on page one. This works in both the web interface\u2019s simple search box and the <code class=\"docutils literal notranslate\"><span class=\"pre\">q</span></code> value of the API.</p><p><a class=\"reference external\" href=\"https://troveconsole.herokuapp.com/v3/?url=https%3A//api.trove.nla.gov.au/v3/result%3Fq%3Dfirstpageseq%3A1%26category%3Dnewspaper%26encoding%3Djson&amp;amp;comment=\"><img alt=\"Try it!\" src=\"https://troveconsole.herokuapp.com/static/img/try-trove-api-console.svg\"/></a></p>", "a[href=\"#asking-critical-questions\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Asking critical questions<a class=\"headerlink\" href=\"#asking-critical-questions\" title=\"Link to this heading\">#</a></h1><p>Focus here is on the \u2018simple search\u2019 option as the parameters are much the same between the web interface and API, and it\u2019s possible to replcate all \u2018Advanced\u2019 search options.</p><p>Topics:</p>", "a[href=\"../newspapers-and-gazettes/data/pages.html#get-page-identifier-from-search\"]": "<h3 class=\"tippy-header\" style=\"margin-top: 0;\"><a class=\"toc-backref\" href=\"#id11\" role=\"doc-backlink\">Get the identifier of a specific page by searching for articles</a><a class=\"headerlink\" href=\"#get-the-identifier-of-a-specific-page-by-searching-for-articles\" title=\"Link to this heading\">#</a></h3><p>To get the identifier of a specific page, you need to construct a very precise search for articles published on that page using the <code class=\"docutils literal notranslate\"><span class=\"pre\">/result</span></code> endpoint. You can do this by specifying the title (the newspaper name), the publication date, and the page number. Once you have the search results, you can extract the page identifier from the article records.</p><p>The newspaper title is set using the <code class=\"docutils literal notranslate\"><span class=\"pre\">l-title</span></code> parameter. For example, the identifier for the <em>Canberra Times</em> is <code class=\"docutils literal notranslate\"><span class=\"pre\">11</span></code>, so to limit your search to the <em>Canberra Times</em> you\u2019d set <code class=\"docutils literal notranslate\"><span class=\"pre\">l-title</span></code> to <code class=\"docutils literal notranslate\"><span class=\"pre\">11</span></code>.</p>"}
+skip_classes = ["headerlink", "sd-stretched-link", "sd-rounded-pill"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
